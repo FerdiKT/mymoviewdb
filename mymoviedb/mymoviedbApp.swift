8f6013ceac3e67c70bc13538359ce5fa6ad82f9c
Ferdi Kızıltoprak <ferdi@arafas.com>
2020-11-29 08:05:58 +0300

    Initial Commit

---
+++ mymoviedb/mymoviedbApp.swift
@@ -0,0 +1,17 @@
+//
+//  mymoviedbApp.swift
+//  mymoviedb
+//
+//  Created by Ferdi Kızıltoprak on 29.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct mymoviedbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
